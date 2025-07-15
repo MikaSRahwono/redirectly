b0f4cbee07eca3f883d7aaf77fca914458dc2969
MikaSRahwono <70706750+MikaSRahwono@users.noreply.github.com>
2025-07-15 21:52:12 +0700

    fix: comments route 53 for now

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,21 @@
 variable "aws_region" {
-  default = "ap-southeast-1"
+    default = "ap-southeast-1"
+}
+
+variable "project_name" {
+    default = "redirectly"
 }
 
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+    default = "10.0.0.0/16"
 }
 
 variable "ddb_table_name" {
-  default = "shortened_links"
+    default = "shortened_links"
 }
 
 variable "s3_bucket_name" {
-  default = "mika-admin-dashboard"
+    default = "mika-admin-dashboard"
 }
 
 variable "create_zip_path" {}
@@ -20,33 +24,39 @@ variable "auth_zip_path" {}
 variable "stats_zip_path" {}
 
 variable "lambda_env_variables" {
-  type = map(string)
+    type = map(string)
 }
 
 variable "cognito_pool_name" {
-  default = "url-shortener-users"
+    default = "url-shortener-users"
 }
 
 variable "cognito_domain_prefix" {
-  default = "mika-url-auth"
+    default = "mika-url-auth"
 }
 
 variable "cognito_callback_urls" {
-  type    = list(string)
-  default = ["https://admin.mikasrahwono.link/callback"]
+    type    = list(string)
+    default = ["https://admin.mikasrahwono.link/callback"]
 }
 
 variable "cognito_logout_urls" {
-  type    = list(string)
-  default = ["https://admin.mikasrahwono.link"]
+    type    = list(string)
+    default = ["https://admin.mikasrahwono.link"]
 }
 
 variable "route53_zone_id" {}
 variable "redirect_domain" {
-  default = "mikasrahwono.link"
+    default = "mikasrahwono.link"
 }
 variable "admin_domain" {
-  default = "admin.mikasrahwono.link"
+    default = "admin.mikasrahwono.link"
+}
+variable "environment" {
+    default = "dev"
+}
+variable "created_by" {
+    default = "mikasrahwono"
 }
 
 variable "api_gateway_domain_name" {}
